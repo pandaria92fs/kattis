dc029b87078174706f21754168ddfc66504860f3
陈献辉xianhuichen <xianhuichen@clubfactory.com>
2020-05-28 20:08:47 +0800

    a

---
+++ src/what.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class what {
+    public static void main(String[] args) {
+        List <String> getInput = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextLine()){
+            getInput.add(scanner.nextLine());
+        }
+
+    }
+    private String cal(){
+    }
+}
